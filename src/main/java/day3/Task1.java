4e14ce19af50463cd9f6b3c11d0d2b0154720238
kssadomtsev <sadomcev.k@gmail.com>
2020-09-23 20:51:19 +0300

    complete day3 tasks

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,40 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        boolean marker = true;
+        while (marker) {
+            String city = scanner.nextLine();
+            switch (city) {
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                case "Stop":
+                    marker = false;
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
