42ca4e848c4ff5c012d26b4ade39ba551326ddee
kssadomtsev <sadomcev.k@gmail.com>
2020-09-25 13:55:03 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,46 @@
 package day5;
 
+import java.time.Year;
+
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setYearProduction(Year.of(1980));
+        car.setColor("Black");
+        car.setModel("A8");
+        System.out.println("Car attributes.");
+        System.out.println("Year of production: " + car.getYearProduction());
+        System.out.println("Color: " + car.getColor());
+        System.out.println("Model: " + car.getModel());
+    }
+}
 
+class Car {
+    private Year yearProduction;
+    private String color;
+    private String model;
+
+    public Year getYearProduction() {
+        return yearProduction;
+    }
+
+    public void setYearProduction(Year yearProduction) {
+        this.yearProduction = yearProduction;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,7 +1,38 @@
 package day5;
 
+import java.time.Year;
+
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(Year.of(1975), "Black", "Ural");
+        System.out.println("Motorbike attributes.");
+        System.out.println("Year of production: " + motorbike.getYearProduction());
+        System.out.println("Color: " + motorbike.getColor());
+        System.out.println("Model: " + motorbike.getModel());
 
+    }
+}
+
+class Motorbike {
+    private Year yearProduction;
+    private String color;
+    private String model;
+
+    public Motorbike(Year yearProduction, String color, String model) {
+        this.yearProduction = yearProduction;
+        this.color = color;
+        this.model = model;
+    }
+
+    public Year getYearProduction() {
+        return yearProduction;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
