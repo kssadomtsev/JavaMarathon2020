8fa3a8a6f4f37576097bd25f40f2dbc0044a9362
kssadomtsev <sadomcev.k@gmail.com>
2020-09-30 18:27:31 +0300

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,11 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Student student = new Student("Bob", "1A");
+        Teacher teacher = new Teacher("Alice", "Art");
+        System.out.println(student.getGroup());
+        System.out.println(teacher.getSubject());
+        student.printInfo();
+        teacher.printInfo();
     }
 }
