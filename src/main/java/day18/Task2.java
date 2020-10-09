72c6a6cc61a9b7814ee181bb7e3db76c07cc85be
kssadomtsev <sadomcev.k@gmail.com>
2020-10-09 14:36:55 +0300

    complete day18 tasks

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -6,15 +6,16 @@ package day18;
 public class Task2 {
     public static void main(String[] args) {
         System.out.println(count7(717771237)); // 5
+        System.out.println(count7(17771237));
+        System.out.println(count7(1213212312));
+        System.out.println(count7(7));
+        System.out.println(count7(0));
     }
 
     public static int count7(int number) {
-        if (number == 0)
+        if (number == 0) {
             return 0;
-
-        if (number % 10 == 7)
-            return 1 + count7(number / 10);
-        else
-            return count7(number / 10);
+        }
+        return (number % 10 == 7 ? 1 : 0) + count7(number / 10);
     }
 }
